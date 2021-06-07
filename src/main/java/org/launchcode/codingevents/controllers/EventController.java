a8865b195599ecefe202d5fba42e8bf9b0b22b61
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-06-07 10:15:08 -0500

    Added fragments to connect the code between different paths to make it cleaner and easier to read.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +13,17 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();   // List accessible for multiple methods
+
     @GetMapping
     public String displayAllEvents(Model model) {  // Handler method for GET requests
-        List<String> events = new ArrayList<>();  // Empty list
-        events.add("Stadium of Fire");
-        events.add("Super Bowl");
-        events.add("Chalk the Block");
-        model.addAttribute("events", events);   // Add list to model
+//        List<String> events = new ArrayList<>();  // Empty list
+//        events.add("Stadium of Fire");
+//        events.add("Super Bowl");
+//        events.add("Chalk the Block");
+//        model.addAttribute("events", events);   // Add list to model
+        model.addAttribute("events", events);
         return "events/index";   // Return template name
     }
 
@@ -25,6 +31,13 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {   // Handler method for POST requests
+        events.add(eventName);
+        return "redirect:";   // redirect: says redirect them to the root path for this specific controller - in this case it will redirect to the displayAllEvents handler and the user will see a list of events
     }
 
 }
