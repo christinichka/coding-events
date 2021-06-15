65d5406da08c36a42f1fed22756a81029496bfea
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-06-15 13:41:08 -0500

    13.5 Exercise completed

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,6 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 /**
@@ -20,14 +18,32 @@ public class Event {
     @Size(max = 500, message = "Description too long!")
     private String description;
 
+    @NotBlank(message="Location cannot be left blank.")
+    @Size(min = 3, max = 50, message= "Location must be between 3 and 50 characters")
+    private String location;
+
+    @AssertTrue
+//    @NotBlank(message="Registration field is required.")
+    private boolean registration;
+
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    @Positive(message="Number of attendees must be one or more.")
+    private int numberOfAttendees;
+
+    @AssertFalse
+    private boolean mealProvided;
+
+    public Event(String name, String description, String location, boolean registration, String contactEmail, int numberOfAttendees, boolean mealProvided) {
         this.name = name;
         this.description = description;
+        this.location = location;
+        this.registration = registration;
         this.contactEmail = contactEmail;
+        this.numberOfAttendees = numberOfAttendees;
+        this.mealProvided = mealProvided;
         this.id = nextId;
         nextId++;
     }
@@ -50,8 +66,40 @@ public class Event {
         this.description = description;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(boolean registration) {
+        this.registration = registration;
+    }
+
     public String getContactEmail() {
         return contactEmail;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
+    }
+
+    public boolean isMealProvided() {
+        return mealProvided;
+    }
+
+    public void setMealProvided(boolean mealProvided) {
+        this.mealProvided = mealProvided;
     }
 
     public void setContactEmail(String contactEmail) {
