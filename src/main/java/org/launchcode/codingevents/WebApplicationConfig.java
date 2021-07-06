7a86c3317d7d4c33b70b5e9a8c0d0c0d4c38efc6
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-07-06 15:38:38 -0500

    Still stuck on getting the BCryptPasswordEncoder to import.

--- src/main/java/org/launchcode/codingevents/WebApplicationConfig.java
+++ src/main/java/org/launchcode/codingevents/WebApplicationConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebApplicationConfig implements WebMvcConfigurer {
@@ -13,6 +15,12 @@ public class WebApplicationConfig implements WebMvcConfigurer {
     public AuthenticationFilter authenticationFilter() {
         return new AuthenticationFilter();
     }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder()
+//    {
+//        return new BCryptPasswordEncoder();
+//    }
 
     // Register the filter with the Spring container
     @Override
